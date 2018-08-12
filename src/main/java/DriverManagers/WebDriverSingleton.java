05a2cdd406cc41bf23e904edfb6da8d61220f609
Ekaterina Moldavskaia <Ekaterina_Moldavskaia@epam.com>
2018-08-12 21:18:55 +0300

    Module 8 with business objects, new project structure and utilities.

---
+++ src/main/java/DriverManagers/WebDriverSingleton.java
@@ -0,0 +1,41 @@
+package DriverManagers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class WebDriverSingleton {
+    private static WebDriver instance;
+
+    private WebDriverSingleton() {
+    }
+
+    public static WebDriver getWebDriverInstance() {
+        if (instance != null) {
+            return instance;
+        }
+        return instance = init();
+    }
+
+    private static WebDriver init() {
+        System.setProperty("webdriver.chrome.driver", "src/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    public static void kill() {
+        if (instance != null) {
+            try {
+                instance.quit();
+            } catch (Exception e) {
+                System.out.println("Cannot kill browser");
+            } finally {
+                instance = null;
+            }
+        }
+    }
+}
---
+++ src/main/java/Tools/Screenshoter.java
@@ -0,0 +1,27 @@
+package Tools;
+
+import DriverManagers.WebDriverSingleton;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Screenshoter {
+    private static final String SCREENSHOTS_NAME_TPL = "screenshots/scr";
+
+    public static void takeScreenshot() {
+        WebDriver driver = WebDriverSingleton.getWebDriverInstance();
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        try {
+            String screenshotName = SCREENSHOTS_NAME_TPL + System.nanoTime();
+            File copy = new File(screenshotName + ".png");
+            FileUtils.copyFile(screenshot, copy);
+            System.out.println("Saved screenshot: " + screenshotName);
+        } catch (IOException e) {
+            System.out.println("Failed to make screenshot");
+        }
+    }
+}
